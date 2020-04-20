ee9529b2d7388a90f64be4a531c103281594e6ee
palminschi <mihaipalminschi@gmail.com>
2020-04-20 19:27:52 +0700

    Add

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import './App.css';
 import Footer from './Footer';
 import Technologies from './Technologies';
+import Header from './Header';
 
 const App = () => {
   return (
     <div>
+      <Header />
       <Technologies />
       <Footer />
-      <div>Hello</div>
     </div>);
 }
 
