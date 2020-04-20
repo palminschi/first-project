4e31e62f5b4838e795584895008d956e2c0cc387
palminschi <mihaipalminschi@gmail.com>
2020-04-21 00:18:06 +0700

    css-module

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import classes from './Header.module.css';
+
 
 const Header = () => {
     return (
-        <header className="header">
+        <header className={classes.header}>
         <img src="https://upload.wikimedia.org/wikipedia/commons/c/c1/Steam_Logo.png"/>
+        <h1>Title Social Network</h1>
         </header>
     )
 }
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,19 +1,20 @@
 import React from "react";
+import classes from './Profile.module.css';
 
 const Profile = () => {
     return (
-    <div className="content">
-        <img id="content-image" src="https://htmlcolorcodes.com/assets/images/html-color-codes-color-tutorials-hero-00e10b1f.jpg"/>
-        <div className="content__ava-descr">
-          <img id="avatar" src="https://i.pinimg.com/originals/0c/a9/e2/0ca9e28dcb12dc698cfd2beda6d6fa64.jpg"/>
+    <div className={classes.content}>
+        <img className={classes.image} src="https://htmlcolorcodes.com/assets/images/html-color-codes-color-tutorials-hero-00e10b1f.jpg"/>
+        <div className={classes.avadescr}>
+          <img className={classes.avatar} src="https://i.pinimg.com/originals/0c/a9/e2/0ca9e28dcb12dc698cfd2beda6d6fa64.jpg"/>
           <div>Description</div>
         </div>
-        <div className="content__newpost">
+        <div className={classes.newpost}>
           New post
         </div>
-        <div className="content__posts">
-          <div>post 1</div>
-          <div>post 2</div>
+        <div className={classes.posts}>
+          <div className={classes.item}>post 1</div>
+          <div className={classes.item}>post 2</div>
         </div>
       </div>
     )}
